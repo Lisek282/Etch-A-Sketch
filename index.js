45d896ec30c0325d27a663ec719d63016783331b
Lisek282 <114082373+Lisek282@users.noreply.github.com>
2023-02-19 21:39:17 +0100

    Add clear canvas function

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 const canvasContainer = document.getElementById("canvasContainer")
+const clearBtn = document.getElementById("clearBtn")
 
+clearBtn.addEventListener('click', clearCanvas)
+
+function clearCanvas(){
+  const allPixels = document.querySelectorAll('.pixel')
+  allPixels.forEach(element => element.style.background = 'white')
+}
 
 function createPixel() {
   const pixel = document.createElement('div')
-
+  
   function draw(){
-    pixel.style.background = 'black'
+    pixel.style.background = "black"
   }
-
+  
   canvasContainer.addEventListener("mousedown", () => {
-      pixel.addEventListener('mousemove', draw)
+    pixel.addEventListener('mousemove', draw)
   })
-
+  
   canvasContainer.addEventListener('mouseup', () => {
     pixel.removeEventListener('mousemove', draw)
   })
-
+  
   pixel.classList.add('pixel')
   canvasContainer.appendChild(pixel)
 }
 
 function createCanvas(columns, rows){
-    const canvasSize = columns * rows
-
-    canvasContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${columns}, auto)`)
-
-    for (let i = 0; i < canvasSize; i++){
-      createPixel()
-    }
+  const canvasSize = columns * columns
+  
+  canvasContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${columns}, auto)`)
+  
+  for (let i = 0; i < canvasSize; i++){
+    createPixel()
+  }
 }
 
-createCanvas(100, 100)
+createCanvas(64)
